a1643e45fe356fb17e113f99dfd61462d202eeb6
Roar022 <krishgarg004@gmail.com>
2024-06-18 13:12:12 +0530

    modified footer component

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
-
 import footerClassNames from "./footerClassNames";
+
 const Footer = () => {
   const {
     container,
@@ -20,43 +20,41 @@ const Footer = () => {
   } = footerClassNames;
 
   return (
-    <div>
-      <footer className={footer}>
-        <div className={container}>
-          <div className={section}>
-            <div className={section1}>
-              <h2 className={section1Heading}>LOGO</h2>
-              <p className={section1Content}>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry.
-              </p>
-            </div>
-            <div className={section2}>
-              <h2 className={section2Heading}>About us</h2>
-              <ul className={section2ul}>
-                <li>
-                  <Link href="#" className={sectionLink}>
-                    Careers
-                  </Link>
-                </li>
-                <li>
-                  <Link href="#" className={sectionLink}>
-                    Blog
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div className={section3}>
-              <h2 className={section3Heading}>Contact us</h2>
-              <p className={section3Content}>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry.
-              </p>
-            </div>
+    <footer className={footer}>
+      <div className={container}>
+        <div className={section}>
+          <div className={section1}>
+            <h2 className={section1Heading}>LOGO</h2>
+            <p className={section1Content}>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+          <div className={section2}>
+            <h2 className={section2Heading}>About us</h2>
+            <ul className={section2ul}>
+              <li>
+                <Link href="#" className={sectionLink}>
+                  Careers
+                </Link>
+              </li>
+              <li>
+                <Link href="#" className={sectionLink}>
+                  Blog
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className={section3}>
+            <h2 className={section3Heading}>Contact us</h2>
+            <p className={section3Content}>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
           </div>
         </div>
-      </footer>
-    </div>
+      </div>
+    </footer>
   );
 };
 
